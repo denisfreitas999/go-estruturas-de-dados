8b7f333e51a8399fba57c924dda2c417d6b006a5
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-08-17 14:55:56 -0300

    feat: :sparkles: Implementação de lista encadeada utilizando a linguagem Go

--- src/main.go
+++ src/main.go
@@ -2,54 +2,148 @@ package main
 
 import "fmt"
 
-type Fila struct {
-	elementos []int
+type No struct {
+	valor   int
+	proximo *No
+}
+type Lista struct {
+	cabeca *No
 }
 
-func inicializaFila() *Fila {
-	return &Fila{elementos: []int{}}
+func NovaLista() *Lista {
+	return &Lista{}
 }
 
-func (f *Fila) Enqueue(valor int) {
-	f.elementos = append(f.elementos, valor)
+func (l *Lista) InserirInicio (valor int) {
+	novoNo := &No{valor: valor, proximo: l.cabeca}
+	l.cabeca = novoNo
 }
 
-func (f *Fila) Dequeue() (int, bool) {
+func (l *Lista) InserirFinal (valor int) {
+	novoNo := &No{valor: valor}
 
-	if len(f.elementos) == 0 {
-		return 0, false
+	if l.cabeca == nil {
+		l.cabeca = novoNo
+	} else {
+		atual := l.cabeca
+		for atual.proximo != nil {
+			atual = atual.proximo
+		}
+		atual.proximo = novoNo
+	}
+}
+
+func (l *Lista) RemoverInicio () (int, bool) {
+	// Lista Vazia
+	if l.cabeca == nil {
+		return 0, false 
+	} else {
+		elementoInicio := l.cabeca
+		l.cabeca = l.cabeca.proximo
+		return elementoInicio.valor, true
+	}
+}
+
+func (l *Lista) RemoverFim () (int, bool) {
+	// Lista Vazia
+	if l.cabeca == nil {
+		return 0, false 
 	}
 
-	primeiroElemento := f.elementos[0]
-	f.elementos = f.elementos[1:]
-	return primeiroElemento, true
-}
-
-func (f *Fila) Peek () (int, bool){
-	if len(f.elementos) == 0 {
-		return 0, false
+	// Lista tem apenas um elemento
+	if l.cabeca.proximo == nil {
+		valorRemovido := l.cabeca.valor
+		l.cabeca = nil
+		return valorRemovido, true
 	}
 
-	return f.elementos[0], true
+	// Lista tem mais de um elemento
+	atual := l.cabeca
+	for atual.proximo.proximo != nil {
+		atual = atual.proximo
+	}
+
+	// Atual é o penúltimo elemento
+	valorRemovido := atual.proximo.valor
+	atual.proximo = nil
+	return valorRemovido, true
 }
 
-func (f *Fila) IsEmpty () (bool){
-	return len(f.elementos) == 0
+func (l *Lista) removerValor(valor int) bool {
+    // Lista vazia
+    if l.cabeca == nil {
+        return false
+    }
+
+    // Valor é o primeiro elemento
+    if l.cabeca.valor == valor {
+        l.cabeca = l.cabeca.proximo
+        return true
+    }
+
+    // O valor não é o primeiro elemento e ele está no meio da lista
+    atual := l.cabeca
+    for atual.proximo != nil && atual.proximo.valor != valor {
+        atual = atual.proximo
+    }
+
+    // Se o próximo nó não for nulo, significa que o valor foi encontrado
+    if atual.proximo != nil {
+        atual.proximo = atual.proximo.proximo
+        return true
+    }
+
+    // Valor não encontrado
+    return false
 }
+
+func (l *Lista) buscarNo (valor int) (bool) {
+	// Verifica se a lista está vazia
+	if l.cabeca == nil {
+		return false
+	}
+
+	// Verifica se o valor é o cabeça
+	if l.cabeca.valor == valor {
+		return true
+	}
+
+	// Verificar por toda a lista
+	atual := l.cabeca
+	for atual.proximo != nil {
+		atual = atual.proximo
+		if atual.valor == valor {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (l *Lista) Imprimir() {
+    atual := l.cabeca
+    for atual != nil {
+        fmt.Print(atual.valor, " ")
+        atual = atual.proximo
+    }
+    fmt.Println(l.cabeca)
+}
+
 
 func main() {
-	newFila := inicializaFila()
-	fmt.Println(*newFila)
-	newFila.Enqueue(1)
-	newFila.Enqueue(2)
-	newFila.Enqueue(3)
-	fmt.Println(*newFila)
-	fmt.Println(newFila.Dequeue())
-	fmt.Println(*newFila)
-	fmt.Println(newFila.Peek())
-	newFila.Enqueue(8)
-	newFila.Enqueue(5)
-	newFila.Enqueue(9)
-	fmt.Println(*newFila)
-	fmt.Println(newFila.IsEmpty())
+	newLista := NovaLista()
+	fmt.Println(newLista)
+	newLista.InserirInicio(1)
+	newLista.InserirFinal(2)
+	newLista.InserirFinal(3)
+	newLista.InserirFinal(4)
+	newLista.InserirFinal(5)
+	newLista.InserirFinal(6)
+	newLista.InserirFinal(7)
+	newLista.InserirFinal(8)
+	newLista.InserirFinal(8)
+	newLista.removerValor(9)
+	newLista.Imprimir()
+	fmt.Println(newLista.buscarNo(2))
+	fmt.Println(newLista.buscarNo(25))
 }
